5bbc2829bd6bc0e376ea9d208e0cd82431628a4f
georgekazz <sfinas1998@hotmail.com>
2023-02-16 02:57:44 +0200

    Basic changes

---
+++ app/src/main/java/com/example/lelermy/ChoiceActivity.kt
@@ -0,0 +1,21 @@
+package com.example.lelermy
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class ChoiceActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choice)
+
+        val stratosksirasbt = findViewById<Button>(R.id.stratosKsirasBt)
+
+        stratosksirasbt.setOnClickListener {
+
+        }
+
+    }
+}
