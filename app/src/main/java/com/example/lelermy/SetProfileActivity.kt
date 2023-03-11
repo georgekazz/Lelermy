c76d1df280bee67d120578f051f9d7d2b44218d6
georgekazz <sfinas1998@hotmail.com>
2023-03-11 21:27:00 +0200

    Set Profile

--- app/src/main/java/com/example/lelermy/SetProfileActivity.kt
+++ app/src/main/java/com/example/lelermy/SetProfileActivity.kt
@@ -2,6 +2,10 @@ package com.example.lelermy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
 class SetProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +14,18 @@ class SetProfileActivity : AppCompatActivity() {
 
         val choiceCounter = intent.getIntExtra("choiceCounter", 0)
         System.out.println(choiceCounter)
+
+        supportFragmentManager.beginTransaction().add(R.id.fragment_container, MyFragment()).commit()
+
     }
+}
+
+
+class MyFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+
+        return inflater.inflate(R.layout.activity_set_profile, container, false)
+    }
+
 }
